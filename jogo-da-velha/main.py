5b25481ce6c40b956a450b6daf8dc085227caa3a
CodigoDeSenior <migueljnm840@gmail.com>
2021-01-19 15:03:13 -0300

    feat: game e player variaveis para armazenar os cliques e de quem e a vez

--- jogo-da-velha/main.py
+++ jogo-da-velha/main.py
@@ -2,6 +2,10 @@ import sys, pygame, random
 from draw_game_board import draw_game_board
 from verify_if_click_is_inside_the_square_and_return_the_center import verify_if_click_is_inside_the_square_and_return_the_center
 
+#Python
+game = ['','','','','','','','','']
+player = 0
+#Pygame 
 pygame.init()
 
 size = width, height = 1080, 720 # WARNING DON'T CHANGE THIS!!!
@@ -16,5 +20,13 @@ draw_game_board(screen, colors["black"], 2)
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.MOUSEBUTTONUP: print(verify_if_click_is_inside_the_square_and_return_the_center(pygame.mouse.get_pos()))
+        if event.type == pygame.MOUSEBUTTONUP:
+            if player % 2 == 0: 
+                game[verify_if_click_is_inside_the_square_and_return_the_center(pygame.mouse.get_pos())[1]] = 'x'
+                print(game)
+                player += 1 
+            else:
+                game[verify_if_click_is_inside_the_square_and_return_the_center(pygame.mouse.get_pos())[1]] = 'o'
+                print(game)
+                player += 1 
     pygame.display.flip()
